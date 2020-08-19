949f9ec206f00007406875559bf9606d5ab2556e
liigalized <liga.baikova@gmail.com>
2020-08-19 04:28:03 +0000

    Clean up index.js file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,16 @@
-let astroHeading = $('.astro-about-heading');
+// Declaring variables
+let astroHeading = $( '.astro-about-heading' );
 
-astroHeading.click(function(){
-    astroHeading.removeClass('heading-active');
-    $(this).addClass('heading-active');
+// Selects heading and fades in the paragraph attached to it + fades out the previous
+astroHeading.click( function( ) {
+    astroHeading.removeClass( 'heading-active' );
+    $( this ).addClass( 'heading-active' );
 
-
-    let idString = $(this).attr('id');
-    let idSplit = idString.split("-");
-    let newId = `#${idSplit[0]}-description`;
+    let idString = $( this ).attr( 'id' );
+    let idSplit = idString.split( "-" );
+    let newId = `#${ idSplit[0] }-description`;
     
-    $('.astro-about-description').removeClass('description-active').hide();
-    $(newId).hide().fadeIn(1500).addClass('description-active');
-})
+    $( '.astro-about-description' ).removeClass( 'description-active' ).hide( );
+    $( newId ).hide( ).fadeIn( 1500 ).addClass( 'description-active') ;
+});
 
