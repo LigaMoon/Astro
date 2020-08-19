12f4699f78395c7c2397a2fe7a5bb4326d251fda
liigalized <liga.baikova@gmail.com>
2020-08-19 04:24:19 +0000

    Clean up zodiac.js file

--- assets/scripts/zodiac.js
+++ assets/scripts/zodiac.js
@@ -1,22 +1,23 @@
 // ------------------------ZODIAC PAGE AND HOROSCOPE API-----------------------
 
-
+// Declaring variables
 let buttons = $('.graphic-button');
-//add a click event to all buttons
-buttons.on('click', function( ) {
-   let zodiac = $(this).attr('id');
-    newRequest(zodiac);
-})
 
-// http request function
+// Adds text retrieved from API into selected elements
+function getData( description, number, color, zodiacDate ) {
+    $('#horoscope-text').text(description);
+    $('#lucky-number').text(number);
+    $('#lucky-color').text(color);
+    $('.horoscope-date').text(zodiacDate);
+}
+
+// Http request function retrieving API data
 function newRequest ( zodiac ) {
     let request = new XMLHttpRequest( );
-    
     request.open("POST", `https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${zodiac}&day=today`);
     request.setRequestHeader("x-rapidapi-host", "sameer-kumar-aztro-v1.p.rapidapi.com");
     request.setRequestHeader("x-rapidapi-key", "95ca44922bmshb87c3bdfad18970p138ae0jsn84bca49e4ea1");
     request.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-    
     request.onload = function( ) {
         var data = JSON.parse( this.response );
         if(request.readyState == 4 && request.status == 200 ) {
@@ -24,22 +25,19 @@ function newRequest ( zodiac ) {
             let number = data.lucky_number;
             let color = data.color;
             let zodiacDate = data.date_range;
-
             getData( description, number, color, zodiacDate);
-
         } else {
             console.log('error');
         }
-    }
+    };
     request.send( );
 }
 
-function getData( description, number, color, zodiacDate ) {
-    $('#horoscope-text').text(description);
-    $('#lucky-number').text(number);
-    $('#lucky-color').text(color);
-    $('.horoscope-date').text(zodiacDate);
-}
+// Add a click event to all buttons on the page and call the NewRequest function on id values of those buttons
+buttons.on('click', function( ) {
+   let zodiac = $(this).attr('id');
+    newRequest(zodiac);
+});
 
 
 
